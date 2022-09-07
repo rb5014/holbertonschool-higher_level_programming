b0ab9dd277b54c99145732854838ddfd574bb378
rb5014 <5014@holbertonschool.com>
2022-09-07 14:04:36 +0200

    1-last_digit.py fixed: use of modulo

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-lastdigit = int(repr(number)[-1])
-if number < 0:
-    lastdigit = -lastdigit
+number = int(random.randint(-10000, 10000))
+lastdigit = number % 10 if number >= 0 else number % -10
 print(f"Last digit of {number} is {lastdigit}", end=' ')
 if lastdigit > 5:
     print("and is greater than 5")
