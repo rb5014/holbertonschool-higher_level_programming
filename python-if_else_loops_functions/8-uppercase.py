3e50bc7c6b1cc27793ab32e14540d388594ea587
rb5014 <5014@holbertonschool.com>
2022-09-07 23:16:09 +0200

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,7 @@
 def uppercase(str):
 
     for c in str:
-        if ord(c) in range(97, 123):
-            print("{}" .format(c))
-            
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{:s}" .format(c), end="")
+    print()
