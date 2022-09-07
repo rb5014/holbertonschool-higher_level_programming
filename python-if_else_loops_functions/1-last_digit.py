3052c3d1247548215f577c9ed493257cd22f7ffa
rb5014 <5014@holbertonschool.com>
2022-09-07 12:29:22 +0200

    1-last_digit.py case 0 fixed

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ print(f"Last digit of {number} is {lastdigit}", end=' ')
 if lastdigit > 5:
     print("and is greater than 5")
 elif lastdigit == 0:
-    print("and is zero")
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")
 int(repr(number)[-1])
