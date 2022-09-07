1034b7bf01b153d511075c5373d42c899bb9307c
rb5014 <5014@holbertonschool.com>
2022-09-07 22:21:16 +0200

    8/home/romain5014/.pyenv/versions/3.8.5/bin/python /home/romain5014/holbertonschool-higher_level_programming/python-if_else_loops_functions/8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,5 @@ def uppercase(str):
 
     for c in str:
         if ord(c) in range(97, 123):
-            print(chr(ord(c) - 32), end='')
-        else: 
-            print(c, end='')
-    print()
+            print("{}" .format(c))
+            
