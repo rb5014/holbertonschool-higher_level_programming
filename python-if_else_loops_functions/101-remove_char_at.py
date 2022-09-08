df61b4c957cea5ce1631bf831ee5c59fa95bfc00
rb5014 <5014@holbertonschool.com>
2022-09-08 12:27:21 +0200

    101-remove_char_at.py

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return str[:n] + (str[n + 1:] if n > 0 else str[n:])
+    if n == 0:
+        cpystr = str[1:]
+    elif n > 0:
+        cpystr = str[:n] + str[n + 1:]
+    else:
+        cpystr = str
+    return cpystr
