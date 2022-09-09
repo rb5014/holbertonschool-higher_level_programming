382cf32452854620cb397c2895e6e373fc7dd49a
rb5014 <5014@holbertonschool.com>
2022-09-09 18:35:01 +0200

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    __import__("hidden_4")
+    for i in range(0, len(dir("hidden_4"))):
+        if dir("hidden_4")[i][:2] != "__":
+            print(dir("hidden_4")[i])
