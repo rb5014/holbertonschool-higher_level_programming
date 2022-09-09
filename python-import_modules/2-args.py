6a27826c37360b829fd18d449f03aab408d1bb30
rb5014 <5014@holbertonschool.com>
2022-09-09 17:55:30 +0200

    2-args.py fixed len number

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     elif len(argv) == 2:
         print("1 argument:")
     else:
-        print(len(argv), "arguments:")
+        print(len(argv) - 1, "arguments:")
     for i in range(1, len(argv)):
         print("{:d}: {:s}" .format(i, argv[i]))
