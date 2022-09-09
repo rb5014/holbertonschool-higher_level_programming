411267e98e37dbe0673b46a40cb35bc50c5f28b4
rb5014 <5014@holbertonschool.com>
2022-09-09 18:37:01 +0200

    4/home/romain5014/.pyenv/versions/3.8.5/bin/python /home/romain5014/holbertonschool-higher_level_programming/python-import_modules/4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    __import__("hidden_4")
+    __import__("hidden_4.pyc")
     for i in range(0, len(dir("hidden_4"))):
-        if dir("hidden_4")[i][:2] != "__":
+        if dir("hidden_4.pyc")[i][:2] != "__":
             print(dir("hidden_4")[i])
