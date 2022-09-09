7bea54d81d313721c6ca8638aac4754953cdada6
rb5014 <5014@holbertonschool.com>
2022-09-09 19:44:01 +0200

    5/home/romain5014/.pyenv/versions/3.8.5/bin/python /home/romain5014/holbertonschool-higher_level_programming/python-import_modules/100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    from calculator_1 import add, sub, mul, div
+    func = add, sub, mul, div
+    op = "+", "-", "*", "/"
+
+    for i in range(1, 4):
+        if argv[2] == op[i]:
+            print(argv[1], op[i], argv[3], "=", func(argv[1], argv[3]))
+            exit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
