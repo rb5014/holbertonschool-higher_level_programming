51d92fcd5e547fcaa330fd399600a90699411c3b
rb5014 <5014@holbertonschool.com>
2022-09-09 17:54:47 +0200

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-import sys
-print(sys.argv)
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+    else:
+        print(len(argv), "arguments:")
+    for i in range(1, len(argv)):
+        print("{:d}: {:s}" .format(i, argv[i]))
