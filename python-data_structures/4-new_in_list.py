3cb5591388748e580f46a466ae0a1272be409b82
rb5014 <5014@holbertonschool.com>
2022-09-13 21:35:56 +0200

    4 actually fixed, cpy assigned before condition

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    cpy = my_list.copy()
     if idx >= 0 and idx < len(my_list):
-        global cpy
-        cpy = my_list.copy()
         cpy[idx] = element
     return(cpy)
