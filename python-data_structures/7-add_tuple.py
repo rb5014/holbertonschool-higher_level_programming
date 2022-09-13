f4268eea2d27fbeade40ff2100e6abae4fc3bd22
rb5014 <5014@holbertonschool.com>
2022-09-13 22:22:41 +0200

    7 fixed

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-from hashlib import new
-from operator import ne
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
     a, b = len(tuple_a), len(tuple_b)
     new_tup = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
