af115b183bb527df450c4507f3d6dd29b30e84c9
rb5014 <5014@holbertonschool.com>
2022-09-13 22:49:54 +0200

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    true_or_false = my_list.copy()
+    for i in range(0, len(my_list)):
+        true_or_false[i] = True if my_list[i] % 2 == 0 else False
+    return true_or_false
