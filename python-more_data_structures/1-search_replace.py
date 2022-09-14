49dabea84f60583241d11fddbcd0d5d5153c6d27
rb5014 <5014@holbertonschool.com>
2022-09-15 00:35:46 +0200

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+'''
+Write a function that replaces all occurrences of an element by another in a new list.
+
+Prototype: def search_replace(my_list, search, replace):
+my_list is the initial list
+search is the element to replace in the list
+replace is the new element
+You are not allowed to import any module
+'''
+
+
 def search_replace(my_list, search, replace):
-    
+    newList = list(map(lambda x: str(x).replace(str(search), str(replace)), my_list))
+    return newList
