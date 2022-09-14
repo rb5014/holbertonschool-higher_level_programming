9ab5cc9843028b70a9f18ccb31edc8d598458988
rb5014 <5014@holbertonschool.com>
2022-09-15 01:07:10 +0200

    task 5

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''
+Function that returns the number of keys in a dictionary.
+
+Prototype: def number_keys(a_dictionary):
+You are not allowed to import any module
+'''
+
+
 def number_keys(a_dictionary):
+    return len(a_dictionary)
