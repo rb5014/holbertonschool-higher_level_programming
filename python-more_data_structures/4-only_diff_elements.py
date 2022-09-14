87d84f676dc6ea91df65fcc1f7b79d300203d775
rb5014 <5014@holbertonschool.com>
2022-09-15 01:03:41 +0200

    task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''
+Function that returns a set of all elements present in only one set.
+
+Prototype: def only_diff_elements(set_1, set_2):
+You are not allowed to import any module
+'''
+
+
 def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
