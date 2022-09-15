353c7ab6bb992343c399af0e65d84bf32a046dd5
rb5014 <5014@holbertonschool.com>
2022-09-15 16:23:27 +0200

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+'''Function that returns a key with the biggest integer value.
+
+Prototype: def best_score(a_dictionary):
+You can assume that all values are only integers
+If no score found, return None
+You can assume all students have a different score
+You are not allowed to import any module'''
+
+
+from re import A
+
+
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    sortedValues = sorted(list(a_dictionary.values()))
+    bestScore = sortedValues[-1]
+    for key, value in a_dictionary.items():
+        if bestScore == value:
+            return key
