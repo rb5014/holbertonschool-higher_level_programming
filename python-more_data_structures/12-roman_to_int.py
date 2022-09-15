70a855c7a052b4b9fefe4ed80c570eff5810a8ac
rb5014 <5014@holbertonschool.com>
2022-09-15 18:32:37 +0200

    task 12 fixed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -11,11 +11,12 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     number = 0
-    convDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                'M': 1000}
-    for i in range(roman_string):
-        if roman_string[i] == convDict[roman_string[i + 1]]:
-            number -= convDict[roman_string[i]]
-        else:
-            number += convDict[roman_string[i]]
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    listD = list(d)
+    for i in range(len(roman_string)):
+        if i + 1 < len(roman_string):
+            if d[roman_string[i + 1]] > d[roman_string[i]]:
+                number -= d[roman_string[i]]
+                continue
+        number += d[roman_string[i]]
     return number
