955a31a8e66c09d1111823787981d6b28350cd00
rb5014 <5014@holbertonschool.com>
2022-09-15 16:49:28 +0200

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+'''Create a function def roman_to_int(roman_string): that converts a Roman
+numeral to an integer.
+
+You can assume the number will be between 1 to 3999.
+def roman_to_int(roman_string) must return an integer
+If the roman_string is not a string or None, return 0'''
+
+
 def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    number = 0
+    convDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                'M': 1000}
+    for i in roman_string:
+        number += convDict[i]
+    return number
