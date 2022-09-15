2180db8c17a041f59968486785cbda01791ab7a6
rb5014 <5014@holbertonschool.com>
2022-09-15 15:36:41 +0200

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+'''Function that returns a new dictionary with all values multiplied by 2
+
+Prototype: def multiply_by_2(a_dictionary):
+You can assume that all values are only integers
+Returns a new dictionary
+You are not allowed to import any module'''
+
+
 def multiply_by_2(a_dictionary):
+    newDict = {key: a_dictionary[key]*2 for key in a_dictionary}
+    return newDict
