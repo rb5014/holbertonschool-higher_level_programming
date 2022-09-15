bc183de34e01c30e33b8ec52c3b12190a3174107
rb5014 <5014@holbertonschool.com>
2022-09-15 10:46:52 +0200

    taslk 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+'''function that prints a dictionary by ordered keys.
+
+Prototype: def print_sorted_dictionary(a_dictionary):
+You can assume that all keys are strings
+Keys should be sorted by alphabetic order
+Only sort keys of the first level
+(don’t sort keys of a dictionary inside the main dictionary)
+Dictionary values can have any type
+You are not allowed to import any module'''
+
+
 def print_sorted_dictionary(a_dictionary):
+    sortedList = sorted(a_dictionary.items())
+    sortedDict = {}
+    for key, value in sortedList:
+        sortedDict[key] = value
+    for key, value in sortedDict.items():
+        print(key, ': ', value)
