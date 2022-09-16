c8cb0940a194ca983c931bb28334e5a738839d97
rb5014 <5014@holbertonschool.com>
2022-09-16 12:34:18 +0200

    task 5 and 6

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''function that raises a name exception with a message.
+
+Prototype: def raise_exception_msg(message=""):
+You are not allowed to import any module'''
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
