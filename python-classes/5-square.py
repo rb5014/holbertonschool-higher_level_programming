fc0adef32ef537163a0fc462ec108a14bc2c9118
rb5014 <5014@holbertonschool.com>
2022-09-19 16:01:12 +0200

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 3-square.py)
+
+Private instance attribute: size:
+property def size(self): to retrieve it
+property setter def size(self, value): to set it:
+size must be an integer, otherwise raise a TypeError exception with
+ the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message
+size must be >= 0
+Instantiation with optional size: def __init__(self, size=0):
+Public instance method: def area(self): that returns the current square area
+You are not allowed to import any module
+"""
+
+
+class Square:
+    """defines a square by: (based on 3-square.py)"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print('#', end='')
+            print()
+        if self.size == 0:
+            print()
