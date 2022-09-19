df3167757d472eb8e547e585f60ba174c664a326
rb5014 <5014@holbertonschool.com>
2022-09-19 14:59:44 +0200

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,8 +3,10 @@
 
 Private instance attribute: size
 Instantiation with optional size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
+size must be an integer, otherwise raise a TypeError exception
+ with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message size
+ must be >= 0
 Public instance method: def area(self): that returns the current square area
 You are not allowed to import any module
 """
@@ -18,6 +20,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
- 
+
     def area(self):
         return self.__size**2
