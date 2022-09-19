d5c94fff8b46a0c66988e00862e20a6786229230
rb5014 <5014@holbertonschool.com>
2022-09-19 14:39:34 +0200

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+"""
+
+
+class Square:
+    """defines a square by: (based on 0-square.py)"""
+    def __init__(self, size):
+        self.__size = size
