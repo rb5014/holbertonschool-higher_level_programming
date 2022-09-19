ec5ecf32486eac7188770900af53f966e4eb44c1
rb5014 <5014@holbertonschool.com>
2022-09-19 16:45:07 +0200

    task 6 condition position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,8 +50,8 @@ class Square:
     @position.setter
     def position(self, value):
         for n in value:
-            if type(n) != int or n < 0:
-                raise TypeError("position must be a tuple of 2" +
+            if len(value) != 2 or type(n) != int or n < 0:
+                raise TypeError("position must be a tuple of 2 " +
                                 "positive integers")
         self.__private = value
 
