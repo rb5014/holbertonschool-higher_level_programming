b180f123534949a57be373deeacea034015d3b79
rb5014 <5014@holbertonschool.com>
2022-09-19 16:33:04 +0200

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class Square that defines a square by: (based on 4-square.py)
+"""class Square that defines a square by: (based on 5-square.py)
 
 Private instance attribute: size:
 property def size(self): to retrieve it
@@ -8,18 +8,27 @@ size must be an integer, otherwise raise a TypeError exception with the
  message size must be an integer
 if size is less than 0, raise a ValueError exception with the message size
  must be >= 0
-Instantiation with optional size: def __init__(self, size=0):
+Private instance attribute: position:
+property def position(self): to retrieve it
+property setter def position(self, value): to set it:
+position must be a tuple of 2 positive integers, otherwise raise a TypeError
+ exception with the message position must be a tuple of 2 positive integers
+Instantiation with optional size and optional position:
+def __init__(self, size=0, position=(0, 0)):
 Public instance method: def area(self): that returns the current square area
 Public instance method: def my_print(self): that prints in stdout the square
- with the character #:
+with the character #:
 if size is equal to 0, print an empty line
+position should be use by using space - Don’t fill lines by spaces when
+ position[1] > 0
 """
 
 
 class Square:
-    """defines a square by: (based on 4-square.py)"""
-    def __init__(self, size=0):
+    """defines a square by: (based on 5-square.py)"""
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -34,11 +43,24 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        return self.__private
+
+    @position.setter
+    def position(self, value):
+        if not all(type(n) == int for n in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__private = value
+
     def area(self):
         return self.__size**2
 
     def my_print(self):
         for i in range(self.size):
+            for k in range(self.position[0]):
+                print(' ', end='')
             for j in range(self.size):
                 print('#', end='')
             print()
