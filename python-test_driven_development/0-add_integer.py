7a0434305bee737a6c290e309396d82c2096a633
rb5014 <5014@holbertonschool.com>
2022-09-20 20:49:00 +0200

    task 0 fixed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,12 +5,11 @@
 
 def add_integer(a, b=98):
     """ function that adds 2 integers"""
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-    if type(a) != int:
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return a + b
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return int(a) + int(b)
