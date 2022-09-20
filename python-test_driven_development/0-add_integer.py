36c7ae63f6ccf0e2c3209178f46042bb54ea6de0
rb5014 <5014@holbertonschool.com>
2022-09-20 21:33:16 +0200

    task 0 test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,9 +4,11 @@
 
 
 def add_integer(a, b=98):
-    """ function that adds 2 integers"""
-    if a is None or (type(a) is not int and type(a) is not float):
+    """function that adds 2 integers"""
+    if a is None or (type(a) is not int and type(a) is not float) or a != a:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float or b != b:
         raise TypeError("b must be an integer")
+    if a == float('inf') or b == float('inf'):
+        raise OverflowError("cannot convert float infinity to integer")
     return int(a) + int(b)
