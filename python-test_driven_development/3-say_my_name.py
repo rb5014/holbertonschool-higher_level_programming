160ac9a723d54109210ea742b7c89cded4cb05a3
rb5014 <5014@holbertonschool.com>
2022-09-21 13:10:06 +0200

    task 4-print_square.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""_summary_ module containing a function say_my_name
+""" Module containing a function say_my_name
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """function that prints My name is <first name> <last name>"""
+    """ say_my_name - prints "My name is <first name> <last name>"
+        Parameters:
+        @first_name: the first name to print
+        @last_name: the last name to print
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
