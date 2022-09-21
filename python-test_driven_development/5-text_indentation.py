2e64b77b46b4b061329b0fef69d3c55f937d76ee
rb5014 <5014@holbertonschool.com>
2022-09-21 16:43:58 +0200

    task 5-test_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+""" Module containing a function text_indentation
+"""
+
+
+def text_indentation(text):
+    """ text_indentation - prints a text with 2 new lines after each of
+        these characters: ., ? and :
+
+        Parameters:
+            @text: text must be a string, otherwise raise a TypeError exception
+            with the message text must be a string
+
+        There should be no space at the beginning or at the end of each
+        printed line
+    """
+    afterDelim = False
+
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if text[i] in ".?:":
+            print(text[i]+"\n")
+            afterDelim = True
+        else:
+            if afterDelim is False or text[i] != " ":
+                print(text[i], end='')
+                afterDelim = False
