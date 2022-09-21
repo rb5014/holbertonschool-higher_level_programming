46f5bb6dedaacb5bbd36fb760502a69d6581841a
rb5014 <5014@holbertonschool.com>
2022-09-21 12:13:20 +0200

    task 3-say_my_name.py fixed no args

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,7 @@
 """
 
 
-def say_my_name(first_name="", last_name=""):
+def say_my_name(first_name, last_name=""):
     """function that prints My name is <first name> <last name>"""
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
