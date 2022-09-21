198bb5508b7309fff34514167c9905d205a4deef
rb5014 <5014@holbertonschool.com>
2022-09-21 11:35:25 +0200

    task 3-say_my_name.py fixed space fgor myt name is Bob

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,6 +10,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    if last_name != "":
-        last_name = " " + last_name
-    print("My name is {:s}{:s}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
