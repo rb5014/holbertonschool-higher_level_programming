9a9ab9285fabb6b0f7c28b2adf4ac8193826dec2
rb5014 <5014@holbertonschool.com>
2022-09-21 11:17:41 +0200

    task 2 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""_summary_ module containing a function say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function that prints My name is <first name> <last name>"""
+    if not first_name:
+        first_name = ""
+    elif type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if not last_name:
+        last_name = ""
+    elif last_name and type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
