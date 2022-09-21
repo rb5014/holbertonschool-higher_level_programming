5e6eb2a8b2b6c21e01b63188e839f013f59cc1fd
rb5014 <5014@holbertonschool.com>
2022-09-21 11:30:51 +0200

    task 3-say_my_name.py fixed missing one arg

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,12 +5,11 @@
 
 def say_my_name(first_name, last_name=""):
     """function that prints My name is <first name> <last name>"""
-    if not first_name:
-        first_name = ""
-    elif type(first_name) != str:
+
+    if first_name is None or type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if not last_name:
-        last_name = ""
-    elif last_name and type(last_name) != str:
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    print("My name is", first_name, last_name)
+    if last_name != "":
+        last_name = " " + last_name
+    print("My name is {:s}{:s}".format(first_name, last_name))
