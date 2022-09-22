39861a45e65a7f701714b5041a32a595f2fdc094
rb5014 <5014@holbertonschool.com>
2022-09-22 19:59:28 +0200

    task 100-singly_linked_list.py

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -32,33 +32,46 @@ class Node:
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
-        
+
     @property
     def data(self):
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         if type(value) != int:
             raise TypeError("data must be an integer")
         else:
             self.__data = value
-            
+
     @property
     def next_node(self):
         return self.__next_node
-    
+
     @data.setter
     def next_node(self, value):
         if type(value) != Node and value:
             raise TypeError("next_node must be a Node object")
         else:
             self.__data = value
-    
-    class SinglyLinkedList:
-        """_summary_ class of the singly linked list
-        """
-        def __init__(self):
-            pass        
-        
-        def sorted_insert(self, value):
+
+
+class SinglyLinkedList:
+    """_summary_ class of the singly linked list
+    """
+    def __init__(self):
+        self.__head = []
+
+    def __str__(self):
+        result = ""
+        flag = False
+        for i in self.__head:
+            if flag is True:
+                result += "\n"
+            result += f"{i}"
+            flag = True
+        return result
+
+    def sorted_insert(self, value):
+        self.__head.append(value)
+        self.__head.sort()
