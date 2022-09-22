0f3224dfe8d92a354ca998e9d966c3e5469200d0
rb5014 <5014@holbertonschool.com>
2022-09-22 17:11:11 +0200

    /home/rb5014/.pyenv/versions/3.8.5/bin/python /home/rb5014/Repositories/holbertonschool-higher_level_programming/python-more_classes/1-main.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module containing a class Rectangle
+"""
+
+class Rectangle:
+	""" Rectangle - defines a rectangle
+	"""
+ 
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This the readme file of the project python-more_classes
