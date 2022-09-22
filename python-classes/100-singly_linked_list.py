85dcc8197f96621e930db4e394d472690eda7121
rb5014 <5014@holbertonschool.com>
2022-09-22 20:42:13 +0200

    100-singly_linked_list.pfixed

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -30,8 +30,8 @@ class Node:
     """_summary_ Class of the node
     """
     def __init__(self, data, next_node=None):
-        self.__data = data
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
@@ -39,25 +39,27 @@ class Node:
 
     @data.setter
     def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
         return self.__next_node
 
-    @data.setter
+    @next_node.setter
     def next_node(self, value):
-        if type(value) != Node and value:
+        if value and type(value) != Node:
             raise TypeError("next_node must be a Node object")
         else:
-            self.__data = value
+            self.__next_node = value
 
 
 class SinglyLinkedList:
     """_summary_ class of the singly linked list
     """
     def __init__(self):
-        self.__head = []
+        self.__head = None
 
     def __str__(self):
         result = ""
