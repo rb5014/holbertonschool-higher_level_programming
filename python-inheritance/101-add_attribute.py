c226413da0adff29fb3c7578c864c099fd829a4a
rb5014 <5014@holbertonschool.com>
2022-09-28 17:10:15 +0200

    101-add_attribute.py fixed docstring

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -4,6 +4,13 @@
 
 
 def add_attribute(obj, attr, val):
+    """_summary_
+
+    Args:
+        obj (_type_): an object to check
+        attr (_type_): the attribute to add
+        val (_type_): value to dd in attribute
+"""
     if not hasattr(obj, '__slots__') and not hasattr(obj, '__dict__')\
             and not hasattr(obj, attr):
         raise TypeError("can't add new attribute")
