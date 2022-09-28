6caf5a6a3f2e9e9b699addcbdd6f9802abc0a899
rb5014 <5014@holbertonschool.com>
2022-09-28 13:57:36 +0200

    4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""_summary_ module that contains the function inherits_from
+    """
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class ;
+    otherwise False
+
+    Args:
+        obj (_type_): obj to check
+        a_class (_type_): class to compare with object
+    """
+
+    return isinstance(obj, a_class) and not type(obj) is a_class
