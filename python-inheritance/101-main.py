7fabdb0269bdd180c73653391c092b3e874494ff
rb5014 <5014@holbertonschool.com>
2022-09-28 17:16:02 +0200

    101-add_attribute.py fixed

--- python-inheritance/101-main.py
+++ python-inheritance/101-main.py
@@ -2,15 +2,11 @@
 add_attribute = __import__('101-add_attribute').add_attribute
 
 class MyClass():
-    pass
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
+     __slots__ = ["first_name"]
 
 try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
+    a = MyClass()
+    add_attribute(a, "hbtn", "Holberton")
     print(a.name)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
