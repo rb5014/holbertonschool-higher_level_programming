80a85f2ac054dd000a4e883b3a71344895d38007
rb5014 <5014@holbertonschool.com>
2022-09-28 11:24:13 +0200

    all files added + 0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""_summary_ module that contains the function lookup(obj)
+    """
+
+
+def lookup(obj):
+    """_summary_ Returns a list object
+
+    Args:
+        obj (_type_): _description_
+    """
+    obj.dir()
