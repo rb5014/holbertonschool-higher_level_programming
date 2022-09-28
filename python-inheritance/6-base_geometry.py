72c43eed430df85072cbf64ff41309c0030d33b4
rb5014 <5014@holbertonschool.com>
2022-09-28 14:05:07 +0200

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""_summary_ module that contains the class BaseGeometry
+    """
+
+
+class BaseGeometry:
+    """  (based on 5-base_geometry.py).
+    """
+    def area(self):
+        """ raises an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
