eafb09ad7b6acd30b0edd597ba600a6957d7f016
rb5014 <5014@holbertonschool.com>
2022-09-29 16:58:44 +0200

    8-class_to_json.py pep8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,7 +5,8 @@
 
 def class_to_json(obj):
     """ returns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean) for JSON serialization of an object
+    (list, dictionary, string, integer and boolean) for JSON serialization
+    of an object
 
     Args:
         obj (_type_): instance of the class
