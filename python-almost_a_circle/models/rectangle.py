79d543b34caa0fe13b57fbfae0501ae44b7eaf80
rb5014 <5014@holbertonschool.com>
2022-10-03 21:55:26 +0200

    task 13 to_dictionnary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,8 @@ class Rectangle(Base):
         else:
             for i in kwargs:
                 exec("%s = %d" % ("self." + i, kwargs[i]))
+
+    def to_dictionary(self):
+        """ returns the dictionary representation of a Rectangle"""
+        return {'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height,
+                'width': self.width}
