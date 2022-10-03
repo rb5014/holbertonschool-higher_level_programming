065bd1ee6e2675b97350bb7412d8d58e9d3413df
rb5014 <5014@holbertonschool.com>
2022-10-03 22:06:40 +0200

    task 15 dictionnary to json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module containing the class Base
 """
+import json
 
 
 class Base:
@@ -18,3 +19,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if not list_dictionaries or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
