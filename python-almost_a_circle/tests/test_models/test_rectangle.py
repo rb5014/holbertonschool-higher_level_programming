47bc3ab950b77cd7fd3688efba5962868d7999c4
rb5014 <5014@holbertonschool.com>
2022-10-03 13:40:27 +0200

    task 2 done fixed tests for rectazngle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,42 +13,29 @@ class TestRectangleClass(unittest.TestCase):
         # rectangle object
         r1 = Rectangle(1, 2)
         self.assertIsInstance(r1, Rectangle)
-
-        r2 = Rectangle(1, 2, 3)
-        self.assertIsInstance(r2, Rectangle)
-
-        r3 = Rectangle(1, 2, 3, 4)
-        self.assertIsInstance(r3, Rectangle)
-
-        r4 = Rectangle("1", 2)
-        self.assertIsInstance(r4, Rectangle)
-
-        r5 = Rectangle(1, "2")
-        self.assertIsInstance(r5, Rectangle)
-
-        r6 = Rectangle(1, 2, "3")
-        self.assertIsInstance(r6, Rectangle)
-
-        r7 = Rectangle(1, 2, 3, "4")
-        self.assertIsInstance(r7, Rectangle)
-
-        r8 = Rectangle(1, 2, 3, 4, 5)
-        self.assertIsInstance(r8, Rectangle)
-
-        r9 = Rectangle(-1, 2)
-        self.assertIsInstance(r9, Rectangle)
-
-        r10 = Rectangle(1, -2)
-        self.assertIsInstance(r10, Rectangle)
-
-        r11 = Rectangle(0, 2)
-        self.assertIsInstance(r11, Rectangle)
-
-        r12 = Rectangle(1, 0)
-        self.assertIsInstance(r12, Rectangle)
-
-        r13 = Rectangle(1, 2, -3)
-        self.assertIsInstance(r13, Rectangle)
-
-        r14 = Rectangle(1, 2, 3, -4)
-        self.assertIsInstance(r14, Rectangle)
+        r1 = Rectangle(1, 2, 3)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 2, 3, 4)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle("1", 2)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, "2")
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 2, "3")
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 2, 3, "4")
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(-1, 2)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, -2)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(0, 2)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 0)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 2, -3)
+        self.assertIsInstance(r1, Rectangle)
+        r1 = Rectangle(1, 2, 3, -4)
+        self.assertIsInstance(r1, Rectangle)
