bedbd049287c50fe2e19f7954823905b9d9d9aee
rb5014 <5014@holbertonschool.com>
2022-10-03 11:47:08 +0200

    all files for tests added and not empty

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""test module for the class base
+"""
+import unittest
+from models.base import Base
+
+
+class TestBaseClass(unittest.TestCase):
+    def test_id(self):
+        # Test with the different possible values of id
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
