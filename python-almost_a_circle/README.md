671ed61f0597e43c2eb7651c866feaf273cbcc5d
rb5014 <5014@holbertonschool.com>
2022-10-03 11:23:04 +0200

    all files added + task 0 done

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+This is the readme file of the project python-almost_a_circle
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module containing the class Base
+"""
+
+
+class Base:
+    """
+    -'base' class of all other classes in this project
+    -The goal of it is to manage id attribute in all future classes
+    and to avoid duplicating the same code (by extension, same bugs)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
