85d664d56265b03ebb32fc81fc3400e970a23a63
rb5014 <5014@holbertonschool.com>
2022-10-03 22:34:26 +0200

    task 16 json string to file fixed test

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,7 +31,6 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
-        if list_objs is not None:
-            list_objs = [obj.to_dictionary() for obj in list_objs]
+        list_objs = [obj.to_dictionary() for obj in list_objs]
         with open(f"{cls.__name__}.json", "w", encoding="utf-8") as file:
             file.write(cls.to_json_string(list_objs))
