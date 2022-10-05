bed9a786753f0887aabb18e6e54dae2c45766815
rb5014 <5014@holbertonschool.com>
2022-10-05 19:34:59 +0200

    testst 1

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,8 +11,9 @@ class TestRectangleClass(unittest.TestCase):
     def test_rectangle_exists(self):
         # Test with the different possible values of the args in
         # rectangle object
-        with self.assertIsInstance(Rectangle(1, 2), Rectangle):
-            self.assertIsNotNone(Rectangle(1, 2))
+        r1 = Rectangle(1, 2)
+        with self.assertIsInstance(r1, Rectangle):
+            self.assertIsNotNone(r1)
         self.assertIsInstance(Rectangle(1, 2, 3), Rectangle)
         self.assertIsInstance(Rectangle(1, 2, 3, 4), Rectangle)
         self.assertIsInstance(Rectangle(1, 2, 3, 4, 5), Rectangle)
