987f6062d8abd09f8c7eafd7ac7e987ba0145f61
rb5014 <5014@holbertonschool.com>
2022-10-07 17:44:12 +0200

    task 18 create fixed number of args different between rect and square

--- python-almost_a_circle/main.py
+++ python-almost_a_circle/main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-""" 100-main """
-from distutils.util import run_2to3
-from models.rectangle import Rectangle
+""" Check """
 from models.square import Square
 
-if __name__ == "__main__":
+input_dict = {'size': 2}
+new_rect = Square.create(**input_dict)
 
-    r1 = Rectangle(10, 7, 2, 8)
-    r2 = Rectangle(2, 4)
-    list_rectangles_input = [r1, r2]
+if new_rect is None:
+    print("Square.create doesn't create a new Square")
+    exit(1)
 
-    Rectangle.save_to_file_csv(list_rectangles_input)
-    Rectangle.load_from_file_csv()
+if type(new_rect) is not Square:
+    print("Square.create doesn't create a new Square: {}".format(new_rect))
+    exit(1)
+
+if new_rect.id != input_dict.get('id', 1):
+    print("New Square doesn't have the right ID: {}".format(new_rect.id))
+    exit(1)
+
+if new_rect.size != input_dict['size']:
+    print("New Square doesn't have the right size: {}".format(new_rect.width))
+    exit(1)
+
+if new_rect.x != input_dict.get('x', 0):
+    print("New Square doesn't have the right x: {}".format(new_rect.x))
+    exit(1)
+
+if new_rect.y != input_dict.get('y', 0):
+    print("New Square doesn't have the right y: {}".format(new_rect.y))
+    exit(1)
+
+print("OK", end="")
