68ff507a74bfe857180cfb0af90c89b8d884846a
rb5014 <5014@holbertonschool.com>
2022-10-09 15:43:41 +0200

    101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+import sys
+
+
+def print_stats(fSize, dict):
+    print(f"File size: {fSize}")
+    for key in sorted(dict.keys()):
+        print(f"{key}: {dict[key]}")
+
+
+codesDict = {}
+fileSize = 0
+count = 0
+
+try:
+    for x in sys.stdin:
+        if count == 10:
+            count = 0
+            print_stats(fileSize, codesDict)
+        fileSize += int(x.split(' ')[8])
+        key = x.split(' ')[7]
+        if key in codesDict:
+            codesDict[key] += 1
+        else:
+            codesDict[key] = 1
+        count += 1
+except KeyboardInterrupt:
+    print_stats(fileSize, codesDict)
+    raise
