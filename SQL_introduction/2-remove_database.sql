c370eff72c67073167b8b1776882ea5fa4307756
rb5014 <5014@holbertonschool.com>
2022-11-03 13:44:37 +0100

    task 2 3 4

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Deletes the database hbtn_0c_0 in your MySQL server.
+-- database hbtn_0c_0 doesn’t exist, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+DROP DATABASE IF EXISTS hbtn_0c_0;
