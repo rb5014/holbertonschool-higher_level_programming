c919571a96512fd9a2e706c9bfdaa21272199a9c
rb5014 <5014@holbertonschool.com>
2022-11-05 17:01:34 +0100

    all tasks finished

---
+++ SQL_introduction/103-max_state.sql
@@ -0,0 +1,2 @@
+-- script that displays the max temperature of each state (ordered by state name).
+SELECT `state`,MAX(`value`) as `max_temp` FROM temperatures GROUP BY `state` ORDER BY `state` ASC;
