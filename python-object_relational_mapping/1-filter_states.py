b41a18b6b1280de69b75b04c01c0a67ba91dc059
rb5014 <5014@holbertonschool.com>
2022-11-08 15:16:14 +0100

    1-filter_states.py pycode fixed

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-1-filter_states - lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+1-filter_states - lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa:
 
-    Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+    Your script should take 3 arguments: mysql username, mysql password
+    and database name (no argument validation needed)
     You must use the module MySQLdb (import MySQLdb)
-    Your script should connect to a MySQL server running on localhost at port 3306
+    Your script should connect to a MySQL server running on localhost
+    at port 3306
     Results must be sorted in ascending order by states.id
     Results must be displayed as they are in the example below
     Your code should not be executed when imported
