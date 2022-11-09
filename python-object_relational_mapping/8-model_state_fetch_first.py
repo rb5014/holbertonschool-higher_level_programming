dbac2bf9757a83470b69d7adbd1c1fd457baf05c
rb5014 <5014@holbertonschool.com>
2022-11-09 16:37:58 +0100

    8-model_state_fetch_first.py

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
+from sys import argv as a
+from model_state import Base, State
+
+from sqlalchemy import create_engine, Table, Column
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(a[1], a[2], a[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).all()
+
+    print("{}: {}".format(result[0].id, result[0].name))
