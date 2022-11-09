f81dbb0fb9936419d7bf1c2ef822c150b334b629
rb5014 <5014@holbertonschool.com>
2022-11-09 18:39:42 +0100

    13-model_state_delete_a.py

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+from sys import argv as a
+from model_state import Base, State
+
+from sqlalchemy import create_engine, Table, Column
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(a[1], a[2], a[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name.contains('a')).all()
+    for obj in result:
+        session.delete(obj)
+    session.commit()
