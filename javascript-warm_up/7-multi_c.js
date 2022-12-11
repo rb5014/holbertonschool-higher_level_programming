49a54cdbbd33736d87a14758d4ebbcbe44760e6d
Romain Bayle <5014@holbertonstudents.com>
2022-12-11 12:00:14 +0100

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-for (let i = 0; i < 5; i++) {
-  console.log('C is fun');
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < 5; i++) {
+    console.log('C is fun');
+  }
 }
