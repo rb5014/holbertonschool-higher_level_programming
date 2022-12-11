9821a73dc8b1e60370b78426482596149c0e5ded
Romain Bayle <5014@holbertonstudents.com>
2022-12-11 20:32:59 +0100

    6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
