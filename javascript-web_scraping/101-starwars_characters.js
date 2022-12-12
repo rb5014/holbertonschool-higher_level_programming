2774829d6f1383518dacda15aeb24af3887a3d32
Romain Bayle <5014@holbertonstudents.com>
2022-12-12 13:59:17 +0100

    101-starwars_characters.js fixed

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -4,11 +4,13 @@ const myUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(myUrl, function (error, response, body) {
   if (error) { console.error('error:', error); } // Print the error if one occurred
   const filmDict = JSON.parse(body);
-  filmDict.characters.forEach(charUrl => {
-    request(charUrl, function (error, response, body) {
-      if (error) { console.error('error:', error); } // Print the error if one occurred
-      const name = JSON.parse(body).name;
-      console.log(name);
+  for (const ch of JSON.parse(body).characters) {
+    request(ch, function (err, response, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(JSON.parse(body).name);
+      }
     });
-  });
+  }
 });
