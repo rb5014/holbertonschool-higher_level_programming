e7ee6d05ae03900fa96a94b8a97eff424bb7a491
Romain Bayle <5014@holbertonstudents.com>
2022-12-12 11:29:11 +0100

    4-starwars_count.js semistandard fixed

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,12 +3,6 @@ const request = require('request');
 const myUrl = process.argv[2];
 request(myUrl, function (error, response, body) {
   if (error) { console.error('error:', error); } // Print the error if one occurred
-  const films = JSON.parse(body);
+  const films = JSON.parse(body).results;
   let count = 0;
-  for (let i = 0; i < films.results.length; i++) {
-    if (films.results[i].characters.filter(character => character.includes('18')).length > 0) {
-      count += 1;
-    }
-  }
-  console.log(count);
 });
