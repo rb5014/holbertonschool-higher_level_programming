27075c9649dfb5c260daeac7f491c9c96315f6b9
Romain Bayle <5014@holbertonstudents.com>
2022-12-12 11:57:24 +0100

    Revert "4-starwars_count.js semistd validation"
    This reverts commit 3809af049dac008aa7545d9f5dea927ceccd38be.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,6 @@ const myUrl = process.argv[2];
 request(myUrl, function (error, response, body) {
   if (error) { console.error('error:', error); } // Print the error if one occurred
   const films = JSON.parse(body);
-  const count = films.results.reduce(function (count, film) {
-    return film.characters.filter(
-        char => char.includes('18')).length > 0 ? count + 1 : count;
-  })
+  const count = films.results.filter(film => film.characters.filter(char => char.includes('18')).length > 0).length;
   console.log(count);
 });
