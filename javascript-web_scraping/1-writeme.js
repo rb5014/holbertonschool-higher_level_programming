aa39f0c564ea842eeae0284900d0647127c0b3bd
Romain Bayle <5014@holbertonstudents.com>
2022-12-12 10:18:11 +0100

    1-writeme.js fixed semistandard

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-data = process.argv[3];
+const data = process.argv[3];
 fs.writeFile(process.argv[2], data, (error) => {
   if (error) { console.log(error); }
 });
