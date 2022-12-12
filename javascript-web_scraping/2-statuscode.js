b838fb967083c4d8d21174ac079018e45fe2b75d
Romain Bayle <5014@holbertonstudents.com>
2022-12-12 10:17:24 +0100

    2-statuscode.js

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) { console.error('error:', error); } // Print the error if one occurred
+  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
+});
