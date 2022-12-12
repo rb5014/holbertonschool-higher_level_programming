01f56295a192e30c0c7123c15f41f3917a8d0117
Romain Bayle <5014@holbertonstudents.com>
2022-12-12 11:42:36 +0100

    4-starwars_count.js text with filter

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,11 +4,6 @@ const myUrl = process.argv[2];
 request(myUrl, function (error, response, body) {
   if (error) { console.error('error:', error); } // Print the error if one occurred
   const films = JSON.parse(body);
-  let count = 0;
-  for (let i = 0; i < films.results.length; i++) {
-    if (films.results[i].characters.filter(character => character.includes('18')).length > 0) {
-      count += 1;
-    }
-  }
-  console.log(count);
+  const count = films.results.filter(film => film.characters.filter(char => char.includes('18')).length > 0).length
+  console.log(count)
 });
